cbd5eb670a0c6896101bd4bcaa2087dd30cad954
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-02-21 10:32:29 +0700

    feat: add expense and income form input

---
+++ expenses/static/expenses/components/RecordInput.vue
@@ -0,0 +1,40 @@
+var RecordInput = Vue.component('record-input', {
+	props: {
+		id: String,
+		label: String,
+		type: String,
+		items: Array,
+		currency: String,
+	},
+	template: '\
+	<div class="col-12">\
+		<template v-if="type === \'string\'">\
+			<span class="label-input"><label :for="id">{{ label }}</label></span>\
+			<span class="input-text">\
+				<div class="input-group input-group-sm">\
+					<input :id="id" type="text" class="form-control">\
+				</div>\
+			</span>\
+		</template>\
+		<template v-else-if="type === \'currency\'">\
+			<span class="label-input"><label :for="id">{{ label }}</label></span>\
+			<span class="input-text">\
+				<div class="input-group input-group-sm mb-3">\
+				  <div class="input-group-prepend">\
+				    <span class="input-group-text" id="basic-addon1">{{ currency }}</span>\
+				  </div>\
+				  <input :id="id" type="text" class="form-control">\
+				</div>\
+			</span>\
+		</template>\
+		<template v-else-if="type === \'dropdown\'">\
+			<span class="label-input"><label :for="id">{{ label }}</label></span>\
+			<span class="input-text">\
+				<select class="custom-select custom-select-sm">\
+				  <option v-for="item in items">{{ item }}</option>\
+				</select>\
+			</span>\
+		</template>\
+	</div>\
+	'
+})

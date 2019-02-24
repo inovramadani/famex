bfe59bce5bd9fae49682d5d3a8cf8d38dd5f8e31
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-02-25 01:15:44 +0700

    refactor: add controlled input functionality

--- expenses/static/expenses/components/RecordInput.vue
+++ expenses/static/expenses/components/RecordInput.vue
@@ -5,6 +5,7 @@ var RecordInput = Vue.component('record-input', {
 		type: String,
 		items: Array,
 		currency: String,
+		value: String,
 	},
 	template: '\
 	<div class="col-12">\
@@ -12,7 +13,7 @@ var RecordInput = Vue.component('record-input', {
 			<span class="label-input"><label :for="id">{{ label }}</label></span>\
 			<span class="input-text">\
 				<div class="input-group input-group-sm">\
-					<input :id="id" type="text" class="form-control">\
+					<input :id="id" type="text" class="form-control" :value="value" @input="$emit(\'input\', $event.target.value)">\
 				</div>\
 			</span>\
 		</template>\
@@ -21,16 +22,16 @@ var RecordInput = Vue.component('record-input', {
 			<span class="input-text">\
 				<div class="input-group input-group-sm mb-3">\
 				  <div class="input-group-prepend">\
-				    <span class="input-group-text" id="basic-addon1">{{ currency }}</span>\
+				    <span class="input-group-text">{{ currency }}</span>\
 				  </div>\
-				  <input :id="id" type="text" class="form-control">\
+				  <input :id="id" type="text" class="form-control" :value="value" @input="$emit(\'input\', $event.target.value)">\
 				</div>\
 			</span>\
 		</template>\
 		<template v-else-if="type === \'dropdown\'">\
 			<span class="label-input"><label :for="id">{{ label }}</label></span>\
 			<span class="input-text">\
-				<select class="custom-select custom-select-sm">\
+				<select class="custom-select custom-select-sm" :value="value" @input="$emit(\'input\', $event.target.value)">\
 				  <option v-for="item in items">{{ item }}</option>\
 				</select>\
 			</span>\
